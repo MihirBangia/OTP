151065e64c6e21c306a23c5882b357bb42aecfa1
Mihir Bangia <89865393+MihirBangia@users.noreply.github.com>
2023-06-09 17:15:49 +0530

    Update otp.php

--- otp.php
+++ otp.php
@@ -33,8 +33,8 @@ function generateotp()
             $mail->isSMTP(); //Send using SMTP
             $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
             $mail->SMTPAuth = true; //Enable SMTP authentication
-            $mail->Username = 'geekytest123@gmail.com'; //SMTP username
-            $mail->Password = 'eiuzoosjhbbsireo'; //SMTP password
+            $mail->Username = 'your email'; //SMTP username
+            $mail->Password = 'your app password'; //SMTP password
             $mail->SMTPSecure = 'tls'; //Enable implicit TLS encryption
             $mail->Port = 587; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
